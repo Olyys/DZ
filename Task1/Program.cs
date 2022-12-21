b02b2271e73ffc5f1fe7e46018e644d87ec605ff
Olya <olya.korablina@gmail.com>
2022-12-21 19:10:20 +0300

    решена первая задача

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//По двум заданным числам проверять является ли первое квадратом второго
+Console.Write("введите первое число: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.Write("введите второе число: ");
+int b = int.Parse(Console.ReadLine()!);
+if (a==b*b)
+{
+    Console.Write ("Перое число является квадратом второго");
+}
+else
+{
+    Console.Write("Первое число не является квадратом второго");
+}
